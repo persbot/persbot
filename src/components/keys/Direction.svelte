399a3ff72f33b01623c9462d79a5665a6c402c3d
Persbot <bizalinantin@gmail.com>
2022-06-02 11:32:45 +0300

    keys direction

--- src/components/keys/Direction.svelte
+++ src/components/keys/Direction.svelte
@@ -5,20 +5,17 @@
   const values: ["horizontal", "vertical"] = ["horizontal", "vertical"];
 
   let group =
-    values.find((it) => it === localStorage.keys_orientation) || values[0];
+    values.find((it) => it === localStorage.keys_direction) || values[0];
 
-  $: document.documentElement.dataset.keys_orientation =
-    localStorage.keys_orientation = group;
+  $: document.documentElement.dataset.keys_direction =
+    localStorage.keys_direction = group;
 </script>
 
 <div class="key">
   <FirstLastChild id="keys" is="first" />
   <span><Icon name="Direction" />Direction</span>
-  {#each values as orientation}
-    <label
-      ><input type="radio" bind:group value={orientation} /><span
-      />{orientation}</label
-    >
+  {#each values as value}
+    <label><input type="radio" bind:group {value} /><span />{value}</label>
   {/each}
 </div>
 
