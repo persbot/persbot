bb95f9fde4aa0b03c45db805b084b79991af97c1
persbot <106700410+persbot@users.noreply.github.com>
2022-06-02 10:55:38 +0300

    Update bundle.prod.ts

--- src/bundle.prod.ts
+++ src/bundle.prod.ts
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
       wb.messageSW({ type: 'SKIP_WAITING' })
 
       const closeLink = document.createElement('a')
-      closeLink.setAttribute('style', 'font-size:large;color:#dc2626;text-decoration:none;font-weight:bold;vertical-align:text-bottom')
+      closeLink.setAttribute('style', 'font-size:large;color:#dc2626;text-decoration:none;font-weight:bold')
       closeLink.href = 'javascript:void(0)'
       closeLink.append('×')
       closeLink.onclick = () => container.remove()
@@ -23,7 +23,7 @@ window.addEventListener('load', () => {
       refreshLink.append('Refresh')
 
       const container = document.createElement('div')
-      container.setAttribute('style', `width:fit-content;color:#111827;position:fixed;bottom:1ex;left:50%;transform:translateX(-50%);background:#e5e7eb;padding:0 .5ex;z-index:100;line-height:1.25em;font-size:smaller;border:1px dotted #9ca3af`)
+      container.setAttribute('style', `width:fit-content;color:#111827;position:fixed;bottom:1ex;left:50%;transform:translateX(-50%);background:#e5e7eb;padding:1ex 1ex .5ex;z-index:100;line-height:1;font-size:smaller;border:1px dotted #9ca3af`)
       container.append(closeLink, ' New version available! ', refreshLink, ' to update.')
       document.body.append(container)
 
