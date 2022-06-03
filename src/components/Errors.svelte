4ce378b6680e4350f8c7d960f25ff9b45987b39c
Persbot <bizalinantin@gmail.com>
2022-06-03 14:38:39 +0300

    scroll in view errors

--- src/components/Errors.svelte
+++ src/components/Errors.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import { afterUpdate } from "svelte";
+
   import { errors } from "../store";
   import Icon from "./icon/Icon.svelte";
+
+  let el: HTMLElement;
+
+  afterUpdate(() => el?.scrollIntoView(false));
 </script>
 
 {#if $errors.length}
-  <div class="key error">
+  <div bind:this={el} class="key error">
     <span><Icon name="Warning" />Error</span>
     {#each $errors as error}
       <span
