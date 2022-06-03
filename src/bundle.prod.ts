fa94289b6a04624750dd8113aabb37f510d28498
Persbot <bizalinantin@gmail.com>
2022-06-03 14:52:22 +0300

    pwa theme

--- src/bundle.prod.ts
+++ src/bundle.prod.ts
@@ -12,18 +12,18 @@ window.addEventListener('load', () => {
       wb.messageSW({ type: 'SKIP_WAITING' })
 
       const closeLink = document.createElement('a')
-      closeLink.setAttribute('style', 'font-size:large;color:#dc2626;text-decoration:none;font-weight:bold')
+      closeLink.setAttribute('style', 'font-size:larger;text-decoration:none;font-weight:bolder;line-height:0.5;vertical-align:bottom')
       closeLink.href = 'javascript:void(0)'
       closeLink.append('×')
       closeLink.onclick = () => container.remove()
 
       const refreshLink = document.createElement('a')
-      refreshLink.setAttribute('style', 'color:#0ea5e9;text-decoration:none')
+      refreshLink.setAttribute('style', 'font-weight:bold;text-decoration:none')
       refreshLink.href = '/'
       refreshLink.append('Refresh')
 
       const container = document.createElement('div')
-      container.setAttribute('style', `width:fit-content;color:#111827;position:fixed;bottom:1ex;left:50%;transform:translateX(-50%);background:#e5e7eb;padding:1ex 1ex .5ex;z-index:100;line-height:1;font-size:smaller;border:1px dotted #9ca3af`)
+      container.setAttribute('style', `width:fit-content;color:#111827;position:fixed;bottom:1ex;left:50%;transform:translateX(-50%);background:#e5e7eb;padding:1ex 1ex .5ex;z-index:100;line-height:1;font-size:smaller;border-radius:.25em;border:1px dotted #9ca3af`)
       container.append(closeLink, ' New version available! ', refreshLink, ' to update.')
       document.body.append(container)
 
